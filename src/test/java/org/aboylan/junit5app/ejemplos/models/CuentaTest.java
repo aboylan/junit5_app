c10c809617dbcafb56aa6954d716eb9ff0d7eb11
Alex Boylan <aboylan@users.noreply.github.com>
2023-11-05 13:17:39 -0600

    Repitiendo pruebas con @RepeatedTest

--- src/test/java/org/aboylan/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/aboylan/junit5app/ejemplos/models/CuentaTest.java
@@ -272,4 +272,18 @@ class CuentaTest {
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
+
+    @DisplayName("Probando debito cuenta repetir")
+    @RepeatedTest(value = 5, name = "{displayName} Repeticion numero {currentRepetition} de {totalRepetitions}")
+    void testDebitoCuentaRepeetir(RepetitionInfo info) {
+
+        if(info.getCurrentRepetition() == 3) {
+            System.out.println("estamos en la repeticion " + info.getCurrentRepetition());
+        }
+        cuenta.debito(new BigDecimal(100));
+
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
 }
